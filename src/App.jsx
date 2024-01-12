e313ff8f878378fc5f75cf03237bc171775ea55f
sksheikh <skuddin.ius@gmail.com>
2024-01-12 19:20:00 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from './components/Layout';
+
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <h1 className='text-center mt-4'>Blog App</h1>
+      </Layout>
+    </>
+  )
+}
+
+export default App
